e137847fa2e525751ec8e57dab5386fa1770fafe
poorvikaaa <poorvikaml207@gmail.com>
2025-11-22 19:06:09 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+class ExampleScene extends Phaser.Scene {
+  preload() {}
+  create() {}
+  update() {}
+  ball;
+  preload() {
+    this.load.image("ball", "img/ball.png");
+  }
+   create() {
+    this.ball = this.add.sprite(50, 50, "ball");
+  }
+  
+}
+
+const config = {
+  type: Phaser.CANVAS,
+  width: 480,
+  height: 320,
+  scene: ExampleScene,
+    scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  backgroundColor: "#eeeeee",
+
+};
+
+const game = new Phaser.Game(config);
